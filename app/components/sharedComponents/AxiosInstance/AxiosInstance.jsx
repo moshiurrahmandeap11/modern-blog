af7ba4c0c9922fb39a831d0eec7e1266021f42b7
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-02-17 12:28:55 +0600

    dep

--- app/components/sharedComponents/AxiosInstance/AxiosInstance.jsx
+++ app/components/sharedComponents/AxiosInstance/AxiosInstance.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
-    timeout: 10000,
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://server-blog-weld.vercel.app/api",
     headers: {
         "Content-Type": "application/json",
     },
